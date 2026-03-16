0e6c4b631e740a4e8a36a415e161edeb9e0b3470
dkstjwls06 <love09010224@gmail.com>
2026-03-16 14:21:58 +0900

    week2

--- src/ch02/miniProject/IsRectCollision.java
+++ src/ch02/miniProject/IsRectCollision.java
@@ -1,4 +1,32 @@
 package ch02.miniProject;
 
+import java.util.Scanner;
+
 public class IsRectCollision {
+    public static void main(String[] args){
+        int x1, y1, x2, y2, w1, w2, h1, h2;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("사각형 A의 좌상단 좌표 (x1, y1), 너비, 높이: ");
+        x1= sc.nextInt();
+        y1 = sc.nextInt();
+        w1=sc.nextInt();
+        h1=sc.nextInt();
+        System.out.print("사각형 B의 좌상단 좌표 (x2, y2), 너비, 높이: ");
+        x2= sc.nextInt();
+        y2 = sc.nextInt();
+        w2=sc.nextInt();
+        h2=sc.nextInt();
+
+//        if(((x2 - x1 < w1)&&(y2 - y1 < h1)) || ((x1 - x2 < w2)&&(y2 - y1 < h1)) || ((x1 - x2 < w2)&&(y1 - y2 < h2)) || ((x2 - x1 < w1)&&(y1 - y2 < h2))){
+//            System.out.println("사각형이 겹칩니다.");
+//        } else {
+//            System.out.println("사각형이 겹치지 않습니다.");
+//        }
+        if((x1 + w1 <= x2) || (x2 + w2 <= w1) || (y1 + h1 <= y2) || (y2 + h2 <= y1)){
+            System.out.println("사각형이 겹치지 않습니다.");
+        } else {
+            System.out.println("사각형이 겹칩니다.");
+        }
+    }
 }
--- src/ch02/miniProject/isCircCollision.java
+++ src/ch02/miniProject/isCircCollision.java
@@ -1,4 +1,23 @@
 package ch02.miniProject;
 
+import java.util.Scanner;
+
 public class isCircCollision {
+    public static void main(String[] args){
+        int x, y, r, x1, y1;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("원의 중심(x,y) 좌표와 반지름을 입력하세요: ");
+        x= sc.nextInt();
+        y = sc.nextInt();
+        r = sc.nextInt();
+        System.out.print("점의 (x,y) 좌표와 반지름을 입력하세요: ");
+        x1= sc.nextInt();
+        y1 = sc.nextInt();
+        if(Math.pow((x-x1),2) + Math.pow((y-y1),2) <= Math.pow(r,2)){
+            System.out.printf("점 (%d, %d)는 원 위에 있습니다.\n", x1, y1);
+        } else {
+            System.out.printf("점 (%d, %d)는 원 위에 있지 않습니다.\n", x1, y1);
+        }
+    }
 }
